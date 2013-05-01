9df6b7451915e4771f839c2dfa0ee1be4155ea7a
iqbalmineraltown <iqbalmineraltown@gmail.com>
2013-05-01 20:29:45 +0700

    posisi center condition green

--- XTS_AES/src/main.java
+++ XTS_AES/src/main.java
@@ -1,4 +1,6 @@
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -12,17 +14,15 @@ import javax.swing.JFrame;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author juan.karsten
  */
 public class main {
+
     /**
      * @param args the command line arguments
      */
-    
-    
     public static void main(String[] args) throws IOException {
 //        try {
 //            RandomAccessFile file=new RandomAccessFile("D:\\a.txt","rw");
@@ -63,15 +63,19 @@ public class main {
 //        } catch (FileNotFoundException ex) {
 //            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
 //        }
-        
-       XTSAESDialog xTSAESDialog=new XTSAESDialog(null, true);
-       
-       xTSAESDialog.setVisible(true);
-        
+
+        XTSAESDialog xTSAESDialog = new XTSAESDialog(null, true);
+        final Toolkit toolkit = Toolkit.getDefaultToolkit();
+        final Dimension screenSize = toolkit.getScreenSize();
+        final int x = (screenSize.width - xTSAESDialog.getWidth()) / 2;
+        final int y = (screenSize.height - xTSAESDialog.getHeight()) / 2;
+        xTSAESDialog.setLocation(x, y);
+        xTSAESDialog.setVisible(true);
+
     }
-    
-    public static void printMatrix(byte [] a){
-        for(int ii=0;ii<a.length;ii++){
+
+    public static void printMatrix(byte[] a) {
+        for (int ii = 0; ii < a.length; ii++) {
             System.out.println(a[ii]);
         }
     }
